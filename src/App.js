f374d2ab825860fb03efbd661d7be40252c6b217
Brendan Hamill <brenhamill@gmail.com>
2018-03-16 14:26:36 -0400

    add basic react files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">GITHUB USER SEARCH</h1>
-        </header>
-        <p className="App-intro">
-          <input type="text" />
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">GITHUB USER SEARCH</h1>
+          </header>
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { thunk } from '../util/thunk';
+import rootReducer from '../reducers/rootReducer';
+
+const middlewares = [thunk, logger];
+
+const configureStore = ( preloadedState = {} ) => (
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
+);
+
+export default configureStore;
